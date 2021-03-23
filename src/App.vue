f1c6cd8fb8aff62bcf94d0cba078dc8648d8d2d2
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-23 18:05:39 +0900

    add: button component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="p-8">
+    <Button>Click me!</Button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Button from "@/components/Button/Button";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: { Button },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
