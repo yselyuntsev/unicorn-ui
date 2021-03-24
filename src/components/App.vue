f1ca67ee116a7d4472a63672387034ff5aed8782
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-24 11:05:49 +0900

    Create App.vue

---
+++ src/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="unicorn-app">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss" scoped>
+.unicorn-app {
+  @apply flex;
+}
+</style>
