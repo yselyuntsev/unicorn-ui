519526c774f00e51171e355ae2c9f96eae2cc6d0
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-24 14:28:38 +0900

    Create lib.js

---
+++ src/lib.js
@@ -0,0 +1,19 @@
+import "@/assets/index.css";
+
+import * as components from "@/components";
+
+const ComponentLibrary = {
+  install(Vue) {
+    for (const componentName in components) {
+      const component = components[componentName];
+
+      Vue.component(component.name, component);
+    }
+  },
+};
+
+export default ComponentLibrary;
+
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(ComponentLibrary);
+}
