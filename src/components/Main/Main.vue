8fc8d7a5586849d22f700aa5a77068a63a331fb3
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-24 11:05:53 +0900

    Create Main.vue

---
+++ src/components/Main/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="unicorn-main">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style lang="scss" scoped>
+.unicorn-main {
+  @apply flex-1;
+}
+</style>
