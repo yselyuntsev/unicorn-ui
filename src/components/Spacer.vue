06be92286d97a347bf48aed31990d7a3152b556b
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-25 09:45:11 +0900

    Create Spacer.vue

---
+++ src/components/Spacer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="unicorn-spacer"></div>
+</template>
+
+<script>
+export default {
+  name: "Spacer",
+};
+</script>
+
+<style lang="scss" scoped>
+.unicorn-spacer {
+  @apply flex-1;
+}
+</style>
