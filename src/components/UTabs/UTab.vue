1a5275ef86834c1b1d420bf24c0db1bbe04c01de
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-26 11:31:33 +0900

    Add Tabs component

---
+++ src/components/UTabs/UTab.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="unicorn-tab"
+    :class="{ active }"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UTab",
+
+  props: {
+    active: { type: Boolean },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/mixins";
+
+.unicorn-tab {
+  @apply inline-flex h-12 relative items-center px-4;
+  @apply text-gray-500 font-medium uppercase;
+  @apply transition-all;
+
+  &:after {
+    content: "";
+    @apply absolute h-0.5 bg-purple-600 left-0 right-0 bottom-0;
+    @apply transform-gpu scale-x-0 transition-all;
+  }
+
+  @include hover {
+    &:hover {
+      @apply bg-gray-50 cursor-pointer;
+    }
+  }
+
+  &.active {
+    @apply text-purple-600;
+
+    @include hover {
+      &:hover {
+        @apply bg-purple-100;
+      }
+    }
+
+    &:after {
+      @apply scale-x-100;
+    }
+  }
+}
+</style>
---
+++ src/components/UTabs/UTabsItems.vue
@@ -0,0 +1,44 @@
+<script>
+import UTabsItem from "@/components/UTabs/UTabsItem";
+
+export default {
+  name: "UTabsItems",
+
+  model: {
+    prop: "selectedTab",
+    event: "update",
+  },
+
+  components: {
+    UTabsItem,
+  },
+
+  props: {
+    selectedTab: { type: Number },
+  },
+
+  render(h) {
+    return h("div", { class: "unicorn-tabs-items" }, [
+      h("transition", { props: { name: "tabs", mode: "out-in" } }, [
+        h(UTabsItem, {}, [this.$slots.default[this.selectedTab]]),
+      ]),
+    ]);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.unicorn-tabs-items {
+  @apply w-full mt-4;
+}
+
+.tabs-enter-active,
+.tabs-leave-active {
+  @apply transition-all;
+}
+.tabs-enter,
+.tabs-leave-to {
+  @apply opacity-0;
+  @apply transform-gpu -translate-y-2;
+}
+</style>
