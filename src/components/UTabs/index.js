2c9eeee3e28d1b9e0f1ba0dc6efa6ccf5e62e9a3
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-26 12:39:34 +0900

    Create index.js

---
+++ src/components/UTabs/index.js
@@ -0,0 +1,6 @@
+import UTab from "@/components/UTabs/UTab";
+import UTabs from "@/components/UTabs/UTabs";
+import UTabsItem from "@/components/UTabs/UTabsItem";
+import UTabsItems from "@/components/UTabs/UTabsItems";
+
+export { UTab, UTabs, UTabsItem, UTabsItems };
