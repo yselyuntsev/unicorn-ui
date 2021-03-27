c9a17f24d460d45ea97c3fedbfd148630d66acc9
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-27 11:56:31 +0900

    Update UCollapse.vue

--- src/components/UCollapse/UCollapse.vue
+++ src/components/UCollapse/UCollapse.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="unicorn-collapse" :class="{ expanded }">
     <div class="unicorn-collapse__header" @click="toggle">
-      <u-icon icon="mdi-chevron-down" class="unicorn-collapse__icon" />
       <slot name="header"></slot>
     </div>
 
@@ -12,11 +11,8 @@
 </template>
 
 <script>
-import UIcon from "@/components/UIcon";
-
 export default {
   name: "UCollapse",
-  components: { UIcon },
 
   data() {
     return {
@@ -38,23 +34,15 @@ export default {
   @apply transition-all;
 
   &__header {
-    @apply pl-8;
+    @apply pl-8 cursor-pointer;
   }
 
   &__body {
     @apply pt-8;
   }
 
-  &__icon {
-    @apply transition-all absolute left-0;
-  }
-
   &.expanded {
     @apply py-4 border-t border-b;
-
-    .unicorn-collapse__icon {
-      @apply transform-gpu -rotate-90;
-    }
   }
 }
 </style>
