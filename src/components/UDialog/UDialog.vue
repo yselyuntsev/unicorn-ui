24354e560737a8eaecb61a9772545443a9a2ccb1
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-03-30 23:05:21 +0900

    Add persistent property for Dialog component

--- src/components/UDialog/UDialog.vue
+++ src/components/UDialog/UDialog.vue
@@ -2,12 +2,13 @@
   <div class="unicorn-dialog">
     <slot name="activator" :open="open"></slot>
 
-    <transition name="fade" @before-enter="showContent = true">
+    <transition name="fade" appear @before-enter="beforeEnter">
       <div @click.self="close" class="unicorn-dialog__body" v-if="show">
-        <transition name="zoom" @before-leave="show = false">
+        <transition name="zoom" @before-leave="beforeLeave">
           <div
             v-if="showContent"
             class="unicorn-dialog__content"
+            :class="{ pulse: animatePersistent }"
             :style="{ width: width + 'px', maxWidth: width + 'px' }"
           >
             <slot></slot>
@@ -22,23 +23,62 @@
 export default {
   name: "UDialog",
 
+  model: {
+    prop: "value",
+    event: "update",
+  },
+
   props: {
+    value: { type: Boolean, default: null },
     width: { type: [String, Number] },
+    persistent: Boolean,
   },
 
   data() {
     return {
       show: false,
       showContent: false,
+      animatePersistent: false,
     };
   },
 
+  watch: {
+    value: {
+      handler(newValue) {
+        if (newValue) {
+          this.show = true;
+        } else {
+          this.showContent = false;
+        }
+      },
+      immediate: true,
+    },
+  },
+
   methods: {
+    beforeEnter() {
+      this.showContent = true;
+      this.$emit("update", true);
+    },
+
+    beforeLeave() {
+      this.show = false;
+      this.$emit("update", false);
+    },
+
     open() {
       this.show = true;
     },
 
     close() {
+      if (this.persistent) {
+        this.animatePersistent = true;
+        setTimeout(() => {
+          this.animatePersistent = false;
+        }, 150);
+        return;
+      }
+
       this.showContent = false;
     },
   },
@@ -46,6 +86,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@keyframes pulse {
+  from {
+    transform: scale3d(1, 1, 1);
+  }
+
+  50% {
+    transform: scale3d(1.05, 1.05, 1.05);
+  }
+
+  to {
+    transform: scale3d(1, 1, 1);
+  }
+}
+
 .unicorn-dialog {
   @apply relative flex;
 
@@ -57,6 +111,10 @@ export default {
 
   &__content {
     @apply w-full;
+  }
+
+  & .pulse {
+    animation: pulse 150ms ease-in-out;
   }
 }
 
