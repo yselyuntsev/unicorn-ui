48637fab4be2634f8d168f72476a0fc9ffe46b91
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-04-08 00:29:36 +0900

    Create Avatar component

---
+++ src/components/UAvatar/UAvatar.js
@@ -0,0 +1,46 @@
+import "./UAvatar.scss";
+import { convertToUnit } from "../../utils/convertToUnit";
+
+export default {
+  name: "u-avatar",
+
+  props: {
+    left: Boolean,
+    right: Boolean,
+    size: {
+      type: [Number, String],
+      default: 48,
+    },
+  },
+
+  computed: {
+    classes() {
+      const classes = {
+        "u-avatar--left": this.left,
+        "u-avatat--right": this.right,
+      };
+
+      return classes;
+    },
+
+    styles() {
+      const styles = {
+        height: convertToUnit(this.size),
+        width: convertToUnit(this.size),
+        minWidth: convertToUnit(this.size),
+      };
+
+      return styles;
+    },
+  },
+
+  render(h) {
+    const data = {
+      staticClass: "u-avatar",
+      class: this.classes,
+      style: this.styles,
+    };
+
+    return h("div", data, this.$slots.default);
+  },
+};
