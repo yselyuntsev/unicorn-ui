cb134d8853eece50c94aa30be2cb6e71fe1c301a
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-04-08 00:28:33 +0900

    Add utils

---
+++ src/utils/getSlot.js
@@ -0,0 +1,8 @@
+export function getSlot(vm, name = "default", data, optional = false) {
+  if (vm.$scopedSlots[name]) {
+    return vm.$scopedSlots[name](data instanceof Function ? data() : data);
+  } else if (vm.$slots[name] && (!data || optional)) {
+    return vm.$slots[name];
+  }
+  return undefined;
+}
