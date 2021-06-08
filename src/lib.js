7fff5ffc5a6c652f2fc0efb80e7cb7a3c666d598
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-06-08 13:56:14 +0900

    update: components

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,6 @@
-import "@/assets/index.css";
+import "./assets/index.css";
 
-import * as components from "@/components";
+import * as components from "./components";
 
 const ComponentLibrary = {
   install(Vue) {
---
+++ src/new_components/index.js
@@ -0,0 +1,26 @@
+// Components
+export { default as UAlert } from "./UAlert.vue";
+export { default as UApp } from "./UApp.vue";
+export { default as UAppBar } from "./UAppBar.vue";
+export { default as UAvatar } from "./UAvatar.vue";
+export { default as UBottomNavigation } from "./UBottomNavigation.vue";
+export { default as UBottomNavigationItem } from "./UBottomNavigationItem.vue";
+export { default as UBtn } from "./UBtn.vue";
+export { default as UCard } from "./UCard.vue";
+export { default as UCol } from "./UCol.vue";
+export { default as UDialog } from "./UDialog.vue";
+export { default as UDismissButton } from "./UDismissButton.vue";
+export { default as UDivider } from "./UDivider.vue";
+export { default as UDropdown } from "./UDropdown.vue";
+export { default as UGroupHeader } from "./UGroupHeader.vue";
+export { default as UMain } from "./UMain.vue";
+export { default as URow } from "./URow.vue";
+export { default as USelect } from "./USelect.vue";
+export { default as USidebar } from "./USidebar.vue";
+export { default as USidebarGroup } from "./USidebarGroup.vue";
+export { default as USidebarItem } from "./USidebarItem.vue";
+export { default as USpacer } from "./USpacer.vue";
+export { default as UTextField } from "./UTextField.vue";
+
+// HOC
+export { default as UWithBadge } from "../hoc/UWithBadge.vue";
