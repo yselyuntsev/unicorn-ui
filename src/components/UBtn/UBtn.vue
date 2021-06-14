7ef17c40c303afa43e3041a1013adcd05530e166
Yuri Selyuntsev <yselyuntsev@gmail.com>
2021-06-14 12:37:16 +0900

    update: UBtn styles

--- src/components/UBtn/UBtn.vue
+++ src/components/UBtn/UBtn.vue
@@ -6,23 +6,31 @@ import URippleEffect from "../URippleEffect/URippleEffect";
 
 export default {
   name: "u-btn",
+
   components: { URippleEffect },
+
   mixins: [variants],
 
   props: {
     dense: Boolean,
+    disabled: Boolean,
     muted: Boolean,
     text: Boolean,
+    rippleEffect: {
+      type: Boolean,
+      default: true,
+    },
   },
 
   computed: {
     classes() {
       return {
         "u-btn": true,
-        [`u-btn--${this.variant}`]: this.variant,
-        "u-btn--dense": this.dense,
-        "u-btn--muted": this.muted,
-        "u-btn--text": this.text,
+        [this.variant]: this.variant,
+        "is-dense": this.dense,
+        "is-muted": this.muted,
+        "is-text": this.text,
+        "is-disabled": this.disabled,
       };
     },
   },
@@ -36,9 +44,9 @@ export default {
 </script>
 
 <template>
-  <button @click="handleClick" :class="classes">
+  <button @click="handleClick" :class="classes" :disabled="disabled">
     <slot></slot>
 
-    <u-ripple-effect />
+    <u-ripple-effect v-if="rippleEffect" />
   </button>
 </template>
